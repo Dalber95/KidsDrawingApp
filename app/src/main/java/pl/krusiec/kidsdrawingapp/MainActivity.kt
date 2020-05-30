3d1314515cf3748585d4619ac4fe8687599ed45a
Kamil <krusiec.kamil82@gmail.com>
2020-05-30 10:36:27 +0200

    Adding The Color Pallet

--- app/src/main/java/pl/krusiec/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/pl/krusiec/kidsdrawingapp/MainActivity.kt
@@ -3,16 +3,30 @@ package pl.krusiec.kidsdrawingapp
 import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.dialog_brush_size.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var imageButtonCurrentPaint: ImageButton? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         drawingView.setSizeForBrush(20.toFloat())
+
+        imageButtonCurrentPaint = llPaintColor[1] as ImageButton
+        imageButtonCurrentPaint!!.setImageDrawable(
+            ContextCompat.getDrawable(
+                this,
+                R.drawable.pallet_selected
+            )
+        )
+
         ibBrush.setOnClickListener {
             showBrushSizeChooserDialog()
         }
